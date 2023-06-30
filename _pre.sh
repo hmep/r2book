1028694225c31a711c2d376aa2842e3350da593d
Tobias Hagberg <tobias@hagberg.com>
2023-06-30 20:48:34 +0200

    render docs 1688150914

--- _pre.sh
+++ _pre.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-for i in forms/*.qmd; do quarto render $i; done
+case $1 in
 
-git add forms/100respekt-*.pdf
+forms)
+  for i in forms/*.qmd; do quarto render $i; done
+  git add forms/100respekt-*.pdf
+;;
+
+*)
+  echo "No forms rendered."
+;;
+
+esac
+
