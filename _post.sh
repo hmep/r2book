b4456fda0346459e9eea29489237b8ff9d6a7afd
Tobias Hagberg <tobias@hagberg.com>
2023-06-30 21:01:11 +0200

    render docs 1688151671

--- _post.sh
+++ _post.sh
@@ -6,6 +6,15 @@ git add docs
 
 git commit -am "render docs $(date +'%s')"
 
-#git push origin main -f
+case $1 in
 
-git push github main -f
+publish)
+  #git push origin main -f
+  git push github main -f
+;;
+
+*)
+  echo "No files published. Add a 'publish' argument to the post-render command in _quarto.yml to publish them."
+;;
+
+esac
