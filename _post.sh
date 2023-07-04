6f70b8d35c2b7e7f236ad4f6843a30a986939260
Tobias Hagberg <tobias@hagberg.com>
2023-07-04 16:31:27 +0200

    render docs 1688481087

--- _post.sh
+++ _post.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cp CNAME docs/
-
 git add docs
-
 git commit -am "render docs $(date +'%s')"
 
 case $1 in
+
+stage)
+  rsync -avz docs staging:
+;;
 
 publish)
   git push origin main -f
@@ -14,7 +16,7 @@ publish)
 ;;
 
 *)
-  echo "No files published. Add a 'publish' argument to the post-render command in _quarto.yml to publish them."
+  echo "No files deployed. Add a 'stage' or publish' argument to the post-render command in _quarto.yml to push them."
 ;;
 
 esac
